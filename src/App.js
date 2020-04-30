d6c58d613f07d08c729a124eec76bd4b6821b3cb
Keita Ito <kta.i.1129@gmail.com>
2020-05-01 02:16:50 +0900

    refs #00000 props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 8},
-    {name: "New Born"}
+    {name: "New Born"},
+    {name: 24, age: "student"}
   ];
   return (
     <div>
@@ -19,6 +21,11 @@ const App = () => {
 
 const User = (props) => {
   return <div>Hi, I am {props.name}, I'm {props.age} old, a student.</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number
 }
 
 User.defaultProps = {
