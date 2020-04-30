278bc05f497346ff11f0ae2f678aa93ab5c79a0e
Keita Ito <kta.i.1129@gmail.com>
2020-05-01 01:42:44 +0900

    refs #00000 components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render(){
-    const dom = (
-      <React.Fragment>
-        <input type="text" onChange={() => {console.log("Hi!!!")}}></input>
-        <h1>fuck you</h1>
-      </React.Fragment>
-    );
-    return dom;
-  }
+//class App extends Component {
+//  render(){
+//    const dom = (
+//      <React.Fragment>
+//        <input type="text" onChange={() => {console.log("Hi!!!")}}></input>
+//        <h1>fuck you</h1>
+//      </React.Fragment>
+//    );
+//    return dom;
+//  }
+//}
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>ねこです</div>
 }
 
 export default App;
